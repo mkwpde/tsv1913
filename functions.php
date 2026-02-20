428aa8e75c5eb41888b8fc0725b9b00dbeafd63c
Micha Krapp <michakrapp@gmail.com>
2026-02-20 12:38:02 +0000

    feat(contact): change recipient

--- functions.php
+++ functions.php
@@ -74,4 +74,19 @@ if ( ! function_exists( 'tsv1913_fix_default_avatar_url' ) ) {
 		return $args;
 	}
 }
-add_filter( 'get_avatar_data', 'tsv1913_fix_default_avatar_url'); 
+add_filter( 'get_avatar_data', 'tsv1913_fix_default_avatar_url');
+
+/**
+ * Get form block recipients from theme mod and return as array.
+ * @param array $recipients Die Empfänger
+ * @param int $form_id Die Formular-ID
+ * @param array $fields Die überprüften Felder
+ * @param array $files Die überprüften Dateien
+ * @since tsv1913 1.2
+ *
+ * @return array
+ */
+function tsv1913_form_block_recipients( array $recipients, int $form_id, array $fields, array $files ): array {
+	return [ 'kontakt@tsv1913.de' ];
+}
+add_filter( 'form_block_recipients', 'tsv1913_form_block_recipients' );
